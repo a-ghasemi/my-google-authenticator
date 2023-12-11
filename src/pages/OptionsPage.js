44d2919cbcfa9e639b102a73ebb1c9681cd46e02
Armin <datarivers.org@gmail.com>
2023-12-11 15:40:22 +0100

    adds pages

---
+++ src/pages/OptionsPage.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+// Import other components like CodeGeneratorForm
+
+const OptionsPage = () => {
+    // State and functions to handle adding new code generators
+    // and other settings would go here
+
+    return (
+        <div className="options-page">
+            <h1>Authenticator Settings</h1>
+            {/* Form to add new code generator */}
+            {/* List current code generators with options to edit or delete */}
+        </div>
+    );
+};
+
+export default OptionsPage;
---
+++ src/pages/PopupPage.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+// Import components like CodeGenerator
+
+const PopupPage = () => {
+    // State to hold the list of code generators
+    // useEffect to load the generators on component mount
+
+    return (
+        <div className="popup-page">
+            <h1>My Codes</h1>
+            {/* Map through the code generators and render them */}
+        </div>
+    );
+};
+
+export default PopupPage;
