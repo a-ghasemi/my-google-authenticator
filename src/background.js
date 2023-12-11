5c709070abb302f7230f79bae3d5b24f62cb4a7e
Armin <datarivers.org@gmail.com>
2023-12-11 16:58:17 +0100

    adds root files

---
+++ src/background.js
@@ -0,0 +1,7 @@
+// Example background script
+browser.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed");
+    // Perform any setup tasks here
+});
+
+// Add more listeners and functions as needed
